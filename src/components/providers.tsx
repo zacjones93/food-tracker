09bc968bd705604d10e9740a3ca7cbf158d4349b
Lubomir Georgiev <lubomir.georgiev.georgiev@gmail.com>
2025-01-10 16:20:44 +0200

    feat: Add NextUI

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,10 +2,17 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { NextUIProvider } from "@nextui-org/react"
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextUIProvider>
+      <NextThemesProvider {...props} attribute="class">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  )
 }
