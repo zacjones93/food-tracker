46d80bc5e331d0b8ecaace40289206731193292a
Lubomir Georgiev <lubomir.georgiev.georgiev@gmail.com>
2025-03-12 01:34:14 +0200

    feat: Update OpenNext (create-cloudflare@2.41.0)

--- src/app/(settings)/settings/settings-form.tsx
+++ src/app/(settings)/settings/settings-form.tsx
@@ -52,6 +52,7 @@ export function SettingsForm() {
       firstName: session?.user.firstName ?? '',
       lastName: session?.user.lastName ?? '',
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session])
 
   if (!session || isLoading) {
