2ec35840f08323238335ffe5fd86e67fac39dc33
Lubomir Georgiev <lubomir.georgiev.georgiev@gmail.com>
2025-04-07 01:23:43 +0300

    fix: Prevent flicker when navigating between pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function BaseLayout({
         />
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
         >
           <TooltipProvider
