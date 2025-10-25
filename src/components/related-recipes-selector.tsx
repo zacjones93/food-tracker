f9bb612c8fdf9f003c32180c8206fc63f7a24453
Zac Jones <zacjones93@gmail.com>
2025-10-24 22:57:01 -0600

    chore: fix build

--- src/components/related-recipes-selector.tsx
+++ src/components/related-recipes-selector.tsx
@@ -22,7 +22,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Badge } from "@/components/ui/badge";
 import { X, ChevronsUpDown, GripVertical } from "@/components/ui/themed-icons";
 import { cn } from "@/lib/utils";
 import { RELATION_TYPES, type RelationType } from "@/schemas/recipe-relation.schema";
